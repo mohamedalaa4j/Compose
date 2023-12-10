7b24dfe94bc18c723478d7046809d6656c31cc8b
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-12-10 17:12:54 +0200

    TextField + OutlinedTextField added

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -6,19 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import ir.kaaveh.sdpcompose.ssp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
 
+        setContent {
 
             Column( // Linearlayout -> vertical
                 modifier = Modifier.fillMaxSize(),
@@ -26,8 +35,9 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center
             ) {
 
-                Greeting()
-
+                MyText()
+                MyTextField()
+                MyOutLinedTextField()
             }
         }
     }
@@ -35,10 +45,50 @@ class MainActivity : ComponentActivity() {
 
 //@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting() {
+fun MyText() {
     Text(
         text = "Hello!",
         color = colorResource(id = R.color.yellow),
         fontSize = 20.ssp
+    )
+}
+
+@Composable
+fun MyTextField() {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    TextField(value = textValue.value,
+        onValueChange = { text ->
+            textValue.value = text
+        },
+        label = { Text(text = "like hint") }
+    )
+}
+
+@Composable
+fun MyOutLinedTextField() {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    OutlinedTextField(
+        value = textValue.value,
+        onValueChange = { text ->
+            textValue.value = text
+        },
+        label = {
+            Text(text = stringResource(id = R.string.hint))
+        },
+
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number
+        ),
+
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedLabelColor = colorResource(id = R.color.yellow),
+            focusedBorderColor = colorResource(id = R.color.yellow)
+        )
     )
 }
