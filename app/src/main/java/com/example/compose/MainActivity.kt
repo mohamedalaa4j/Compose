14b9d0a5c7cc691883422d2e607142a08e7882a7
Mohamed Alaa <mohamedalaa4j@gmail.com>
2023-12-10 17:35:00 +0200

    Button added

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -21,6 +24,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import ir.kaaveh.sdpcompose.sdp
 import ir.kaaveh.sdpcompose.ssp
 
 class MainActivity : ComponentActivity() {
@@ -35,19 +40,19 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center
             ) {
 
-                MyText()
+                MyText(myText.value)
                 MyTextField()
                 MyOutLinedTextField()
+                MyButton()
             }
         }
     }
 }
 
-//@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun MyText() {
+fun MyText(text: String) {
     Text(
-        text = "Hello!",
+        text = text,
         color = colorResource(id = R.color.yellow),
         fontSize = 20.ssp
     )
@@ -91,4 +96,21 @@ fun MyOutLinedTextField() {
             focusedBorderColor = colorResource(id = R.color.yellow)
         )
     )
+}
+
+val myText = mutableStateOf("")
+
+@Composable
+fun MyButton() {
+    Button(
+        onClick = { myText.value = "Button clicked" },
+        colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.yellow)),
+        border = BorderStroke(2.sdp, colorResource(id = R.color.red))
+    ) {
+
+        Column {
+            Text(text = stringResource(id = R.string.button), color = colorResource(id = R.color.blue))
+            Text(text = "smallerText" , fontSize = 6.ssp)
+        }
+    }
 }
